96afdac95d3336f761bdb3be6045c5da69b4e242
Patricia Silva <pscorreo@gmail.com>
2013-08-01 07:27:53 -0500

    added code to correct failing tests

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,5 +1,19 @@
 class BowlingGame
-	def initialize
-		@rolls = []
-	end
+  def initialize
+    @rolls = []
+  end
+
+# Record a roll in the game.
+#
+#pins - The integer number of pins knocked downin this roll.
+#
+#Returns nothing.
+
+  def roll(pins)
+  	@rolls.push(pins)
+  end
+
+  def score
+  	@rolls.reduce(:+)
+  end
 end
